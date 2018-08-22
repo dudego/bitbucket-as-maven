34c6429babdc998c85f4259d4b2263bca06d8a77
Sahil Dudeja <sahil.dudeja@sapient.com>
2018-08-22 12:52:16 +0530

    add uilib

---
+++ uilib/uilibapp/src/main/java/com/bitbucketrepo/asmaven/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bitbucketrepo.asmaven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bitbucketrepo.asmaven.core.R
+import com.bitbucketrepo.asmaven.corelibrary.CoreConstants
+import com.bitbucketrepo.asmaven.uilibrary.UIConstants
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView.text = """Resolving strings:${CoreConstants.coreString}${UIConstants.uiString}"""
+    }
+}
---
+++ uilib/uilibrary/src/main/java/com/bitbucketrepo/asmaven/uilibrary/UIConstants.kt
@@ -0,0 +1,7 @@
+package com.bitbucketrepo.asmaven.uilibrary
+
+class UIConstants {
+    companion object {
+        val uiString = "This is from UI constants"
+    }
+}
